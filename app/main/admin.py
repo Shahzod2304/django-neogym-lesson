98110a70cf6ea4315fade319cd55924fe3b28de7
javoh20 <javoh7806@gmail.com>
2024-01-24 23:40:54 +0500

    Added models, Home, Why, HomeContent, Trainer, Footer

--- app/main/admin.py
+++ app/main/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from main.models import Home
+from .models import *
+
+admin.site.register(Home)
+admin.site.register(Why)
+admin.site.register(HomeContent)
+admin.site.register(Trainer)
+admin.site.register(Footer)
+
 # Register your models here.
--- app/main/models.py
+++ app/main/models.py
@@ -1,3 +1,60 @@
 from django.db import models
 
 # Create your models here.
+class Home(models.Model):
+    home_title = models.CharField(max_length=50)
+    home_disc = models.TextField()
+
+    def __str__(self):
+        return self.home_title
+    
+    class Meta():
+        verbose_name = "Home carusel"
+        verbose_name_plural = "Home carusel"
+    
+class Why(models.Model):
+    img = models.ImageField(upload_to='images/why')
+    why_title = models.CharField(max_length=50)
+    why_disc = models.TextField()
+
+    def __str__(self):
+        return self.why_title
+    
+    class Meta():
+        verbose_name = "Why"
+        verbose_name_plural = "Why"
+    
+class HomeContent(models.Model):
+    home_content_title = models.CharField(max_length=50)
+    home_content_disc = models.TextField()
+
+    def __str__(self):
+        return self.home_content_title
+    
+    class Meta():
+        verbose_name = "Home content"
+        verbose_name_plural = "Home content"
+
+class Trainer(models.Model):
+    full_name = models.CharField(max_length=50)
+    img = models.ImageField(upload_to='images/trainer')
+    facebook = models.URLField()
+    twitter = models.URLField()
+    instagram = models.URLField()
+
+    def __str__(self):
+        return self.full_name
+    
+    
+class Footer(models.Model):
+    location = models.URLField()
+    phone_number = models.CharField(max_length=20)
+    mail = models.CharField(max_length = 50)
+    footer_text = models.CharField(max_length=100)
+
+    def __str__(self):
+        return "Нижняя часть сайта"
+    
+    class Meta():
+        verbose_name = "Footer data"
+        verbose_name_plural = "Footer data"
